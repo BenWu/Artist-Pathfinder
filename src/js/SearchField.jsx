bb355bcbfe9a4e61a071ce0cb42ee769ac893fc2
Ben Wu <bw96412@hotmail.com>
2017-11-24 22:36:34 -0800

    select search results

--- src/js/SearchField.jsx
+++ src/js/SearchField.jsx
@@ -20,6 +20,7 @@ class SearchField extends React.Component {
         if (!name) {
             return;
         }
+        this.setState({selectedArtist: ''});
         console.log('Searching for ' + name);
         xhr.get({uri: `/search/${name}`}, (err, resp, body) => {
             if (err) {
@@ -27,10 +28,73 @@ class SearchField extends React.Component {
             } else {
                 body = JSON.parse(body);
                 const items = body['artists']['items'];
-
                 this.setState({results: items});
             }
         });
+    }
+
+    selectSearchResult(id) {
+        console.log(id);
+        this.setState({selectedArtist: id});
+    }
+
+    renderResults() {;
+        const results = this.state.results;
+        if (!results) {
+            return;
+        }
+        if (results.length === 0) {
+            return <p>{'No results found'}</p>;
+        }
+        const cards = this.state.results.map((artist) => {
+            let image = '';
+            const images = artist['images'];
+            const targetHeight = 100;
+            if (images.length > 0) {
+                let closest = images[0]['height'] - targetHeight;
+                image = images[0]['url'];
+                for (let i = 1; i < images.length; i++) {
+                    const dist = images[i]['height'] - targetHeight;
+                    if (dist < -20) {
+                        break;
+                    } else if (dist < closest) {
+                        closest = dist;
+                        image = images[i]['url'];
+                    }
+                }
+            }
+            let selected = this.state.selectedArtist
+                && this.state.selectedArtist === artist['id'] ? 'selected' : '';
+            return (
+                <div key={this.state.selectedArtist + artist['id']}
+                     className={`result col-sm-6 ${selected}`}
+                     onClick={this.selectSearchResult.bind(this, artist['id'])}>
+                    {image ? <img className='thumb' src={image}/>
+                        : <img className='thumb'/>}
+                    <span className='name'>{artist['name']}</span>
+                </div>
+            )});
+        const rows = [];
+        let currentRow = [];
+        for (let i = 0 ; i < cards.length ; i++) {
+            currentRow.push(cards[i]);
+            if (i % 2 === 1) {
+                rows.push(currentRow);
+                currentRow = [];
+            }
+        }
+        if (currentRow.length > 0) {
+            rows.push(currentRow);
+        }
+        return (
+            <div className='container search-results'>
+                {rows.map((row, i) =>
+                    <div key={i} className='row'>
+                        {row}
+                    </div>
+                )}
+            </div>
+        );
     }
 
     render() {
@@ -44,19 +108,7 @@ class SearchField extends React.Component {
                        onKeyUp={this.onKeyUp}/>
                 <button type="submit" className="btn btn-primary"
                         onClick={this.searchArtist}>Search</button>
-                <div className="search-results">
-                    {(this.state.results ? (
-                        this.state.results.length === 0 ? (
-                            "No results found"
-                        ) : (
-                            this.state.results.map((artist) =>
-                                <p key={artist['id']}>{artist['name']}</p>
-                            )
-                        )
-                    ) : (
-                        ""
-                    ))}
-                </div>
+                {this.renderResults()}
             </div>
         );
     }
